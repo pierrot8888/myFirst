2c1425c4e8d4524f96f7fef210207d778b30ccdd
pierre <pierchenard@gmail.com>
2018-01-01 11:47:29 +1100

    new version

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class HelloWorld {
+
+    public static void main(String args[]){
+        System.out.println("Hello world!");
+        System.out.println("Bonjour le monde");
+        for(int i=0;i<10;System.out.println(i++));
+
+    }
+    
+}
